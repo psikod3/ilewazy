dd679662fca10186db575f9fa99e66427f83865e
psikod3 <psikod3@gmail.com>
2022-11-16 02:51:25 +0100

    Update transform.py

--- transfrom_m/transform.py
+++ transfrom_m/transform.py
@@ -5,7 +5,7 @@ import json
 # [pd.set_option(option, value) for option, value in options.items()]
 
 with open('../mojscrapy/scrap_results_decoded_fixed.json', encoding='utf-8') as f:
-    results_list = json.load(f)
+    results_list = json.load(f)  # results from file are actually a list of dicts
 
 # duplicate check
 # print(len(results_list))
@@ -15,97 +15,77 @@ with open('../mojscrapy/scrap_results_decoded_fixed.json', encoding='utf-8') as 
 # print(len(set(x)))
 # input()
 
-# reorganizing table
-final_dict = {}
-na_zdjeciu = []  # gonna need this for later
-for item in results_list:
-    for key, value in item.items():
-        value['Skład'] = value.pop(key)
-        temp = list(value.keys())
-        temp2 = [item for item in temp if 'na zdjęciu' in item]
-        value['Porcjax'] = value.pop(temp2[0])
-        dict_temp = {}
-        for number, grams in value['100g'].items():
-            dict_temp[value['Skład'][number]] = grams
-        value['100 g'] = dict_temp
-        dict_temp2 = {}
-        for number, grams in value['Porcjax'].items():
-            dict_temp2[value['Skład'][number]] = grams
-        value['Porcja'] = dict_temp2
-        [value.pop(useless_key) for useless_key in ['100g', 'Skład', 'Porcjax']]
-    final_dict.update(item)
+# reorganizing dict
+organized_dict = {}
+for dictionary in results_list:
+    for name_1, name_2 in dictionary.items():
+
+        name_2['Skład'] = name_2.pop(name_1)  # need replace name_2 ind 2nd lvl dict with key with one name
+        key_list = list(name_2.keys())  # temp list of keys
+        key_list_2 = []
+        for item in key_list:
+            if 'na zdjęciu' in item:
+                key_list_2.append(item)
+                # list comp: key_list_2 = [item for item in key_list if 'na zdjęciu' in item]
+        name_2['Porcja'] = name_2.pop(key_list_2[0])  # replacing "blah na zdjeciu" in 2nd dict with key with one name
+
+        dict_temp = {}  # preparation to remove pointless keys in dicts (i.e. "0" , "1", "2") called later indexes
+        for index, kcal_g in name_2['100g'].items():
+            dict_temp[name_2['Skład'][index]] = kcal_g  # makes dict with keys from values of name_2['Skład'] and values
+                                                        # from name_2['100g']
+        name_2['nowe 100g'] = dict_temp  # adding cleaned dict
+
+        dict_temp_2 = {}  # again
+        for index, kcal_g in name_2['Porcja'].items():
+            dict_temp_2[name_2['Skład'][index]] = kcal_g
+        name_2['nowa Porcja'] = dict_temp_2
+
+        [name_2.pop(useless_key) for useless_key in ['100g', 'Skład', 'Porcja']]  # removing useless dicts
+
+        name_2['nowe 100g']['Wielkość porcji'] = key_list_2[0] # saving 'na zdjęciu (X g)' for df
+        name_2['nowa Porcja']['Wielkość porcji'] = key_list_2[0]
+
+    organized_dict.update(dictionary)
+
 
 # moving all keys to tuple, so df will have easy time adding them to multiindex
 #   on the 1st level of index will be product name
 #   2nd - portion size: 100 g or default
 #   3rd - composition (Skład)
 unnested_dict = {}
-for outerKey, innerDict in final_dict.items():
+for outerKey, innerDict in organized_dict.items():
     for innerKey, innerDict2 in innerDict.items():
         for innerKey2, values in innerDict2.items():
             unnested_dict[(outerKey, innerKey, innerKey2)] = values
 
-# 1) removing letters and spaces from values, and changing , to .
-# 2) recreating unnested_dict with cleaned values
-# value_list = list(unnested_dict.values())
-# value_list_cleaned = []
-# for item in value_list:
-#     str(item)
-#     item_cleaned = item.replace(" kcal", "").replace(" g", "").replace(",", ".")
-#     value_list_cleaned.append(item_cleaned)
-#
-# unnested_dict_cleaned = {}
-# count = 0
-# for key, value in unnested_dict.items():
-#     unnested_dict_cleaned[key] = value_list_cleaned[count]
-#     count = count + 1
-
-# print(unnested_dict)
-# print(unnested_dict_cleaned)
-# print('unnested_dict_cleaned', len(unnested_dict_cleaned))
-
-# print('dl na zdj', len(na_zdjeciu))
-# na_zdjeciu_cleaned = []
-# for item in na_zdjeciu:
-#     item_cleaned = item.replace("['na zdjęciu (", "").replace(" g)']", "").replace(",", ".")
-#     na_zdjeciu_cleaned.append(item_cleaned)
-# # print(na_zdjeciu)
-# # print(na_zdjeciu_cleaned)
-# print('dl na zdj_c', len(na_zdjeciu_cleaned))  # dl na zdj_c 5614 OK!
 
 # creating (multiindex) dataframe from dict with tuples as keys
-mdf = pd.DataFrame(unnested_dict_cleaned, index=[0])
+mdf = pd.DataFrame(unnested_dict, index=[0])
 mdf_t = mdf.transpose()  # looks better
 # mdf_t.rename(columns={0: 'kcal / g'}, inplace=True)
 
 df = mdf_t.reset_index()  # converting multiindex to normal df
-
-# reorganizing df
-df['id'] = range(67968)  # setting up unique id for merge
-# print(df.head(10))
-
-na_zdjeciu_df = df[(df['level_1'] == "100 g") & (df['level_2'] == "Błonnik")]  # dl na zdj_df 5607
-print(na_zdjeciu_df)
-print('dl na zdj_df', len(na_zdjeciu_df.index))
-# na_zdjeciu_df['Porcja (g)'] = na_zdjeciu_cleaned
-# print(na_zdjeciu_df.head(10))
+df['id'] = range(len(df.index))  # setting up unique ids for merge
 
 # splitting 100 g related data from 0 column into GRAMY and KCAL
-stog_sklad = df[(df['level_1'] == "100 g") & (df['level_2'] != "Energia")]
-stog_sklad.rename(columns={0: "100 g (skład)"}, inplace=True)
-stog_sklad.drop(columns=['level_0', 'level_1', 'level_2'], inplace=True)
+stog_sklad = df[(df['level_1'] == "nowe 100g") & (df['level_2'] != "Energia")]
+stog_sklad_2 = stog_sklad.rename(columns={0: "100 g (skład)"})  # new df cause inplace=True throws warning
+stog_sklad_2.drop(columns=['level_0', 'level_1', 'level_2'], inplace=True)
 
-stog_en = df[(df['level_1'] == "100 g") & (df['level_2'] == "Energia")]
-stog_en.rename(columns={0: "100 g (kcal)"}, inplace=True)
-stog_en.drop(columns=['level_0', 'level_1', 'level_2'], inplace=True)
+stog_en = df[(df['level_1'] == "nowe 100g") & (df['level_2'] == "Energia")]
+stog_en_2 = stog_en.rename(columns={0: "100 g (kcal)"})
+stog_en_2.drop(columns=['level_0', 'level_1', 'level_2'], inplace=True)
 # print(stog_en.head(10))
 
 df2 = df.merge(stog_sklad, on='id', how='outer')
-# print(df2.head(20))
+print(df2.head(20))
 # print('df2', len(df2.index))
 df3 = df2.merge(stog_en, on='id', how='outer')
-# print(df3.head(20))
+print(df3.head(20))
 # print('df3', len(df3.index))
+
+
+
 
 # splitting of the 100g column
 
